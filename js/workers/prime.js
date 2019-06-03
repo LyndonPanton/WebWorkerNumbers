de93503dd73cb715b902a6a10aff8458427026e8
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-03 20:58:11 +0100

    Wrote logic for worker

--- js/workers/prime.js
+++ js/workers/prime.js
@@ -1,10 +1,10 @@
 var n = 1;
-while (true) {
-	++n;
-	for (var i = 0; i <= Math.sqrt(n); i++) {
-		if (n % i === 0) {
-			continue;
+prime: while (true) {
+	n += 1;
+    for (var i = 2; i <= Math.sqrt(n); i += 1) {
+	    if (n % i == 0) {
+	    	continue prime;
 		}
-		postMessage(n);
 	}
+    postMessage(n);
 }
