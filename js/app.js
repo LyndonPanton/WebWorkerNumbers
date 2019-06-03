61e03714b7393e7145942186f408841930fe6155
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-03 20:18:45 +0100

    Added click event listeners to pattern buttons

--- js/app.js
+++ js/app.js
@@ -8,6 +8,29 @@
 	}
 
 	function init() {
-		console.log("ok");
+		// Prime
+		let primeStart = document.getElementById("pattern-prime-start");
+		let primeStop = document.getElementById("pattern-prime-stop");
+
+		primeStart.addEventListener("click", function() {
+			console.log("prime worker started")
+		});
+
+		primeStop.addEventListener("click", function() {
+			console.log("prime worker stopped");
+		});
+
+
+		// Fibonacci
+		let fibonacciStart = document.getElementById("pattern-fibonacci-start");
+		let fibonacciStop = document.getElementById("pattern-fibonacci-stop");
+
+		fibonacciStart.addEventListener("click", function() {
+			console.log("fibonacci worker started");
+		});
+
+		fibonacciStop.addEventListener("click", function() {
+			console.log("fibonacci worker stoppped");
+		});
 	}
 }());
