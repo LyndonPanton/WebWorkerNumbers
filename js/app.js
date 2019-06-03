d9b2e9d8723d990cdf40462650600affd6d78344
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-03 20:30:43 +0100

    Added pattern display logic
    The response from the service worker is added to the pattern section when received

--- js/app.js
+++ js/app.js
@@ -11,9 +11,19 @@
 		// Prime
 		let primeStart = document.getElementById("pattern-prime-start");
 		let primeStop = document.getElementById("pattern-prime-stop");
+		let primeContainer = document.getElementById("pattern-prime-container")
+		let primeWorker;
 
 		primeStart.addEventListener("click", function() {
-			console.log("prime worker started")
+			// console.log("prime worker started");
+
+			primeWorker = new Worker("/workers/prime.js");
+			primeWorker.addEventListener("message", function(event) {
+				let number = document.createElement("p");
+				let numberText = document.createTextNode(event.data);
+				number.appendChild(numberText);
+				primeContainer.appendChild(number);
+			})
 		});
 
 		primeStop.addEventListener("click", function() {
@@ -24,9 +34,19 @@
 		// Fibonacci
 		let fibonacciStart = document.getElementById("pattern-fibonacci-start");
 		let fibonacciStop = document.getElementById("pattern-fibonacci-stop");
+		let primeContainer = document.getElementById("pattern-fibonacci-container")
+		let fibonacciWorker;
 
 		fibonacciStart.addEventListener("click", function() {
-			console.log("fibonacci worker started");
+			// console.log("fibonacci worker started");
+
+			fibonacciWorker = new Worker("/workers/fibonacci.js");
+			fibonacciWorker.addEventListener("message", function(event) {
+				let number = document.createElement("p");
+				let numberText = document.createTextNode(event.data);
+				number.appendChild(numberText);
+				fibonacciContainer.appendChild(number);
+			})
 		});
 
 		fibonacciStop.addEventListener("click", function() {
