86bfe1534528067186be39f99d371c8d117833c7
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-03 20:14:34 +0100

    Added check for Web Worker usage

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 (function() {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
+
+	if (window.Worker) {
+		init();
+	} else {
+		document.getElementsByTagName("h1")[0].textContent = "Error: Web Workers not supported";
+	}
+
+	function init() {
+		console.log("ok");
+	}
 }());
